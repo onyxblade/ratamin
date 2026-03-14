5fef35185ec22a2d5b18fe92b6c0ac94e9d58206
merely <git@merely.ca>
2026-03-14 17:11:38 -0300

    init

---
+++ lib/ratamin.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ratamin/version"
+
+module Ratamin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ratamin_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ratamin do
+  it "has a version number" do
+    expect(Ratamin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
